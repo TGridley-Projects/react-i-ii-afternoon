c52694fc31d20a23b2441a30fa3624ed529e96a4
Trevor <alutiiq@comcast.net>
2020-07-29 00:38:47 -0700

    some small progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import People from './data.js';
+import DisplayCard from './DisplayCard.js'
 
 
 class App extends Component{
@@ -7,33 +9,37 @@ class App extends Component{
     super()
 
     this.state ={
-      id: 0,
-      name: {},
-      city: "",
-      country: "",
-      employer: "",
-      title: "",
-      favoriteMovies: []
+      people: People,
+      id: 0
     }
   }
 
 
+
+
 render(){
+  let index = this.state.id 
+  const Person = this.state.people[index]
   return(
     <div className="App">
       <div className="Main">
         <div className="Header">
           <h3 className="h3">Home</h3>
         </div>
-        <div className="InfoCard">
-        
+        <div className="InfoCard">       
+          <DisplayCard
+              id = {Person.id}
+              name={Person.name}
+              city={Person.city}
+              country={Person.country}
+              employer={Person.employer}
+              title={Person.title}
+          favoriteMovies={Person.favoriteMovies} />
         </div>
       </div>
     </div>
+    )
+  }
 
-  )
 }
-
-}
-
 export default App;
---
+++ src/DisplayCard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './App.css'
+
+const DisplayCard = (props) => {
+    return <h1 className="recordNumber">{props.id}/25</h1>
+}
+
+export default DisplayCard
